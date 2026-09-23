127b7dc484bfe00d8b8bba5b6b48afc924e95f2c
Wisdom Kara <wiskara1@gmail.com>
2026-09-23 15:17:58 +0100

    Update freeAdsTraining.js

--- kanelinnovations/src/data/freeAdsTraining.js
+++ kanelinnovations/src/data/freeAdsTraining.js
@@ -1,7 +1,7 @@
 // Update the event details and every registration button here.
 export const freeAdsTraining = {
   registrationUrl: 'https://docs.google.com/forms/d/e/1FAIpQLSd1Lnj8meEEue6fdpSzk6RuTZMZADIIwuEJ_sXR3cv1RsDhuA/viewform?usp=publish-editor',
-  schedule: 'Date and time will be communicated in the WhatsApp class updates group.',
+  schedule: '10th October 2026 · 10:00 AM',
   venue: 'Google Meet',
   portrait: `${import.meta.env.BASE_URL}wisdom.jpeg`,
 };
